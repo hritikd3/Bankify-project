0464af9adcd014eead942d493b209fd3cb0b3b90
hritikd3 <hritikdangi3@gmail.com>
2022-12-27 23:44:51 +0530

    added in progress work

--- js/script.js
+++ js/script.js
@@ -58,16 +58,23 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// LECTURES
+//Dom manupulation
+const displayMovements=function(movements){
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
+  containerMovements.innerHTML=''
+  movements.forEach(function(mov,i){
+  
+  const type=  mov > 0 ? 'deposit': 'withdrawal';
+    const html = `
+  <div class="movements__row">
+          <div class="movements__type movements__type--${type}">${i+1} ${type}</div>
+          <div class="movements__value">${mov}</div>
+        </div>
+  `;
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+  containerMovements.insertAdjacentHTML('afterbegin',html)
+  })
+}
 
-/////////////////////////////////////////////////
+
+displayMovements(account1.movements)
