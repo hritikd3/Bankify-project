a12e068e0d7ca8dd7e02e95f8e22f484dae32563
hritikd3 <hritikdangi3@gmail.com>
2022-12-28 21:40:12 +0530

    master shot

--- js/script.js
+++ js/script.js
@@ -31,7 +31,6 @@ const account4 = {
 
 const accounts = [account1, account2, account3, account4];
 
-
 // //Dom manupulation
 // const displayMovements = function (movements) {
 //   containerMovements.innerHTML = '';
@@ -52,9 +51,44 @@ const accounts = [account1, account2, account3, account4];
 
 // displayMovements(account1.movements);
 
-//sorting user names like we have name as ('Hritik Dangi -> hd) we login using it 
-const user= 'Hritik Dangi';
-const userName= user.toLocaleLowerCase().split(' ').map(function(startLetter){
- return startLetter[0]}).join('')
+//sorting user names like we have name as ('Hritik Dangi -> hd) we login using it
+const createUsername = function (accs) {
+  accs.forEach(function (acc) {
+    //here forEach producing a sideEffect where things working without returning
+    acc.userName = acc.owner
+      .toLocaleLowerCase()
+      .split(' ')
+      .map(function (startLetter) {
+        return startLetter[0]; //eska matlab particular array ko pehle split karke  point to its 0th index and take take element,
+      })
+      .join('');
+  });
+};
+createUsername(accounts);
+console.log(accounts);
 
-console.log(userName)
+// // Elements
+// const labelWelcome = document.querySelector('.welcome');
+// const labelDate = document.querySelector('.date');
+// const labelBalance = document.querySelector('.balance__value');
+// const labelSumIn = document.querySelector('.summary__value--in');
+// const labelSumOut = document.querySelector('.summary__value--out');
+// const labelSumInterest = document.querySelector('.summary__value--interest');
+// const labelTimer = document.querySelector('.timer');
+
+// const containerApp = document.querySelector('.app');
+// const containerMovements = document.querySelector('.movements');
+
+// const btnLogin = document.querySelector('.login__btn');
+// const btnTransfer = document.querySelector('.form__btn--transfer');
+// const btnLoan = document.querySelector('.form__btn--loan');
+// const btnClose = document.querySelector('.form__btn--close');
+// const btnSort = document.querySelector('.btn--sort');
+
+// const inputLoginUsername = document.querySelector('.login__input--user');
+// const inputLoginPin = document.querySelector('.login__input--pin');
+// const inputTransferTo = document.querySelector('.form__input--to');
+// const inputTransferAmount = document.querySelector('.form__input--amount');
+// const inputLoanAmount = document.querySelector('.form__input--loan-amount');
+// const inputCloseUsername = document.querySelector('.form__input--user');
+// const inputClosePin = document.querySelector('.form__input--pin');
